e98985418597676c04d287907fe2b9d904243a5b
somewater <mktsz@mail.ru>
2013-02-09 20:43:07 +0400

    fix ferret initializer config

--- config/initializers/ferret.rb
+++ config/initializers/ferret.rb
@@ -1,6 +1,7 @@
 if defined?(ActsAsFerret)
-  ActsAsFerret.define_index(SearchController::INDEX_NAME, {TextPage => {:fields => [:title_ru, :title_en, :body_ru, :body_en]},
-                                                           News => {:fields => [:title_ru, :title_en, :body_ru, :body_en]},
-                                                           Ckeditor::AttachmentFile => {:fields => [:title_ru, :title_en, :description_ru, :description_en]}
+  ActsAsFerret.define_index(SearchController::INDEX_NAME,
+      :models => {TextPage => {:fields => [:title_ru, :title_en, :body_ru, :body_en]},
+                  News => {:fields => [:title_ru, :title_en, :body_ru, :body_en]},
+                  Ckeditor::AttachmentFile => {:fields => [:title_ru, :title_en, :description_ru, :description_en]}
   })
 end
