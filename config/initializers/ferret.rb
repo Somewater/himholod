c44e0bf2c1438f843068bb128785af5e85a6a39c
somewater <mktsz@mail.ru>
2013-02-09 19:49:15 +0400

    initialize ferret in initializer

---
+++ config/initializers/ferret.rb
@@ -0,0 +1,6 @@
+if defined?(ActsAsFerret)
+  ActsAsFerret.define_index(SearchController::INDEX_NAME, {TextPage => {:fields => [:title_ru, :title_en, :body_ru, :body_en]},
+                                                           News => {:fields => [:title_ru, :title_en, :body_ru, :body_en]},
+                                                           Ckeditor::AttachmentFile => {:fields => [:title_ru, :title_en, :description_ru, :description_en]}
+  })
+end
